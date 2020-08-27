d8959c18ec50cdf9bd297c9a862bec858d7e4b39
Juan  Mayorga Navarrete <juanmayorganavarrete@Mac-mini-de-Juan.local>
2020-08-27 10:25:33 +0200

    variables de entorno y conecta el source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,50 @@
 const path = require('path');
-
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    mode: 'development', // TODO condicionar a variable de entorno
-    entry: [
-        './src/index.tsx',
-    ],
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'main.js'
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader'
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
-    ],
-    performance: {
-        hints: process.env.NODE_ENV === 'production' ? "warning" : false
-    },
-    devServer: {
-        open: true
-    }
+module.exports = () => {
 
+    const env = dotenv.config().parsed;
+
+
+    const envKeys = Object.keys(env).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(env[next]);
+        return prev;
+    }, {});
+
+    return {
+        mode: 'production', // TODO condicionar a variable de entorno
+        entry: [
+            './src/index.tsx',
+        ],
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js']
+        },
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'main.js'
+        },
+        module: {
+            rules: [{
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }]
+        },
+        devtool: false,
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html'
+            }),
+            new webpack.DefinePlugin(envKeys),
+            new webpack.SourceMapDevToolPlugin({})
+        ],
+        performance: {
+            hints: process.env.NODE_ENV === 'production' ? "warning" : false
+        },
+        devServer: {
+            open: true
+        }
+
+    }
 }
